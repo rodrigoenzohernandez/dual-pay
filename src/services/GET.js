a55b5e1c1907f9060fdce077499e86b5221dd119
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-12-20 17:44:55 -0300

    add service to do generic fetch

--- src/services/GET.js
+++ src/services/GET.js
@@ -1,1 +1,24 @@
-// Generic fetch to return data
+/**
+ * Generic fetch to return data
+ * @param {string} baseUrl
+ * @param {string} resource For example /bonds
+ * @param {object} config   Axios config
+ * @returns {config} response
+ *
+ */
+
+async function GET(baseUrl, resource, config) {
+  try {
+    const instance = axios.create({
+      baseURL: baseUrl,
+      timeout: 5000,
+    });
+
+    const response = await instance.get(resource, config);
+
+    return response;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
