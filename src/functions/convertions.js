912014e615d744978156f7597e44ed75ca142d6c
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-12-20 15:38:27 -0300

    api call to get GD30C

--- src/functions/convertions.js
+++ src/functions/convertions.js
@@ -5,13 +5,28 @@
  * @returns {int} Returns quantity of nominals that you will have on Balanz
  */
 
-function usdToNominals(amount) {
+async function usdToNominals(amount) {
 
     const facebankTransferCost = 30;
     const balanzCommission = 0.4;
-    const bondBuyPrice = 35.65; //TO DO: api call
-    const percentageLessCommission = 100 - balanzCommission
-    const nominals = Math.round(((amount - facebankTransferCost) / bondBuyPrice) * percentageLessCommission);
-    return nominals
+    
+    try {
+        const instance = axios.create({
+            baseURL: 'http://localhost:3000',
+            timeout: 5000,
+          });
+        
+        const config = { params: {name: 'GD30C'}}
+        const response = await instance.get('/bonds', config);
+        const bondBuyPrice = response.data.value
+        const percentageLessCommission = 100 - balanzCommission
+        const nominals = Math.round(((amount - facebankTransferCost) / bondBuyPrice) * percentageLessCommission);
+
+        return nominals
+
+      } catch (error) {
+        console.error(error);
+        return null
+      }
 
 }
