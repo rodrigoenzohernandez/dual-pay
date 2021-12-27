7c94a2fb6401b262b90b662fe65f0b52ca7158c0
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-12-27 11:17:26 -0300

    include index

--- index.php
+++ index.php
@@ -1,12 +1,2 @@
-<?php include_once("./src/public/index.html"); ?>
-<?php include_once("./src/public/app.js"); ?>
-<?php include_once("./src/public/style-header.css"); ?>
-<?php include_once("./src/public/style.css"); ?>
-<?php include_once("./src/functions/convertions.js"); ?>
-<?php include_once("./src/functions/nominalsToPesosOrUsd.js"); ?>
-<?php include_once("./src/services/GET.js"); ?>
-
-
-
-
+<?php header( 'Location: ./src/public/index.html' ) ;  ?>
 
