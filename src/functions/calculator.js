ce673d00c313c587838b5952981706e86e39c3df
Maria Jose Matar <matarmariajose@gmail.com>
2021-12-28 17:13:35 -0300

    changes requested

--- src/functions/calculator.js
+++ src/functions/calculator.js
@@ -4,11 +4,11 @@ async function calcularSalarioNeto(salarioNeto, porcentaje) {
     const config = { params: { type: "valoresprincipales" } }
     const { DOLAR_SI_API_BASE_URL } = configVariables()
     const response = await GET(DOLAR_SI_API_BASE_URL, '/', config)
-    let valorDolar = 0
+    let usdPrice = 0
 
     if (response) {
-        valorDolar = parseFloat((response.data[0].casa.compra).replace(',', '.'))
+        usdPrice = parseFloat((response.data[0].casa.compra).replace(',', '.'))
     }
-    var total = ((salarioNeto * porcentaje / 100) / valorDolar).toFixed(2);
+    const total = ((salarioNeto * porcentaje / 100) / usdPrice).toFixed(2);
     return total;
 }
