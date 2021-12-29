03c767f3d4b0352c1aa2739249a547827db68924
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-12-29 18:06:44 -0300

    delete toggle

--- src/functions/frontErrorControl.js
+++ src/functions/frontErrorControl.js
@@ -1,6 +1,5 @@
 const alertWarning = document.querySelector('.alertWarning')
 
 function myFunc() {
-    alertWarning.classList.toggle("alertWarning");
-    alertWarning.style.display = "none";
+    alertWarning.style = "display: none"
 }
